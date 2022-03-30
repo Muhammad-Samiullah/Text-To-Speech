13cb56dbe416a206b095a549261abb6be8df79dd
Muhammad Samiullah <samipervaiz7863@gmail.com>
2022-03-30 20:31:51 +0500

    Added Voice Button

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en-US";
+speech.volume = 1;
+speech.rate = 1;
+speech.pitch = 1;
+let voiceIndex = 0;
+
+
+
+function setSpeech() {
+    return new Promise(
+        function (resolve, reject) {
+            let synth = window.speechSynthesis;
+            let id;
+
+            id = setInterval(() => {
+                if (synth.getVoices().length !== 0) {
+                    resolve(synth.getVoices());
+                    clearInterval(id);
+                }
+            }, 10);
+        }
+    )
+}
+voicesList = []
+let s = setSpeech();
+s.then((voices) => {
+    // console.log(voices)
+    voicesList = voices;
+});
+
+
 function textToAudio() {
     let msg = document.getElementById("text-to-speech").value;
-    
-    let speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
-    
     speech.text = msg;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
     
     window.speechSynthesis.speak(speech);
+}
+
+function changeLanguage() {
+    if(voiceIndex < voicesList.length) {
+        voiceIndex += 1;
+    }
+    else {
+        voiceIndex = 0;
+    }
+    speech.voice = voicesList[voiceIndex];
 }
